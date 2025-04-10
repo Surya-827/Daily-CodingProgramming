cfb0a678f47cc3168c288f1651e341a92662f57d
Surya Kiran <63420304+Surya-827@users.noreply.github.com>
2025-04-10 07:12:38 +0000

    Added Code lugic for factorial length

--- Python Practice/FactorialLength.py
+++ Python Practice/FactorialLength.py
@@ -1,7 +1,19 @@
 # Sample Logic 
 
-def fact(n):
-    return *[if n==0 or n==1:return 1 else n*fact(n-1)]
+# def fact(n): return [1 if n==0 or n==1 else n*fact(n-1)][0]
+# def getLenthOfFact(x:int) -> int: print(len(str(x)))
+
+class Solution(object):
+    @decorator
+    def findLengthOfFact(factorial) -> int:
+        def wrapper():
+            print("Before Factorial") #Sample LOG Logic implements here
+            factorial(5)
+            print("After Factorial") #Sample LOG Logic implements here
+        return wrapper
 
 
-print(fact(5))
+if "__name__"=="__main__":
+    obj = Solution()
+    x = 5#int(input())
+    obj.findLengthOfFact(factorial(x))
